409a4c7273d29efb11a90f0b1f8c0d7c659ed162
rr0409Rudy <rr0409@srmist.edu.in>
2026-02-18 15:14:52 +0530

    error correction

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 public class PalindromeCheckerApp {
-    public static  void main (String[] args){
-        System.out.println("Welcome to palindrome Checker App Management System")
+    public static void main (String[] args){
+        System.out.println("Welcome to palindrome Checker App Management System");
                 System.out.println("Modify this logic to usecase 2");
     }
 }

da756c9c542efa0b2ebbd4358e925409314af1cf
rr0409Rudy <rr0409@srmist.edu.in>
2026-02-25 15:15:38 +0530

    UC9

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,70 +1,27 @@
-class Node {
-    char data;
-    Node next;
+import java.util.Scanner;
 
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class SinglyLinkedList {
-    Node head;
-
-    public void add(char c) {
-        Node newNode = new Node(c);
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-        temp.next = newNode;
-    }
-
-    public boolean isPalindrome() {
-        if (head == null || head.next == null) return true;
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-        Node tempSecond = secondHalf;
-        while (tempSecond != null) {
-            if (firstHalf.data != tempSecond.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            tempSecond = tempSecond.next;
-        }
-        return true;
-    }
-
-    private Node reverse(Node node) {
-        Node prev = null;
-        Node current = node;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-        return prev;
-    }
-}
 public class PalindromeCheckerApp {
+
+    private static boolean check(String s, int start, int end) {
+        if (start >= end)
+            return true;
+        if (s.charAt(start) != s.charAt(end))
+            return false;
+        return check(s, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
-        String input = "level";
-        SinglyLinkedList list = new SinglyLinkedList();
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-        boolean isPalindrome = list.isPalindrome();
-        System.out.println("Input string: " + input);
-        System.out.println("Is Palindrome? " + isPalindrome);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? " + result);
+
+        sc.close();
     }
 }

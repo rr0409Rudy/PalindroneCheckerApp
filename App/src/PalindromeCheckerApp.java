a769ff56efe64b7b5e2a14622a4d92a06cf1aa56
rr0409Rudy <rr0409@srmist.edu.in>
2026-02-25 15:36:08 +0530

    UC12

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,32 +1,30 @@
 import java.util.Scanner;
+import java.util.Stack;
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop())
+                return false;
+        }
+        return true;
+    }
+}
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
-        PalindromeService service = new PalindromeService();
-
-        boolean result = service.checkPalindrome(input);
-
+        PalindromeStrategy strategy = new StackStrategy();
+        boolean result = strategy.check(input);
         System.out.println("Input: " + input);
         System.out.println("Is Palindrome? " + result);
-
         sc.close();
-    }
-}
-class PalindromeService {
-
-    public boolean checkPalindrome(String input) {
-
-        int start = 0;
-        int end = input.length() - 1;
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end))
-                return false;
-
-            start++;
-            end--;
-        }
-        return true;
     }
 }

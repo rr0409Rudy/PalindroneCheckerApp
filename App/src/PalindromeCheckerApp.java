baff10b5994c0e16e0cd66a9f24d4fed18b09e45
rr0409Rudy <rr0409@srmist.edu.in>
2026-02-25 15:44:08 +0530

    UC13

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
-import java.util.Stack;
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-class StackStrategy implements PalindromeStrategy {
-    public boolean check(String input) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop())
+public class UseCase13PalindromeCheckerApp {
+    public static boolean isPalindrome(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end))
                 return false;
+            start++;
+            end--;
         }
         return true;
     }
-}
-public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
-        PalindromeStrategy strategy = new StackStrategy();
-        boolean result = strategy.check(input);
+        long startTime = System.nanoTime();
+        boolean result = isPalindrome(input);
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
         System.out.println("Input: " + input);
         System.out.println("Is Palindrome? " + result);
+        System.out.println("Execution Time: " + executionTime + " ns");
         sc.close();
     }
 }
